8222d8da4cc3240dda8ccc518ab0a4355b6b3642
mayurpawar12592 <mayurpawar12592@gmail.com>
2025-03-22 17:59:58 +0530

    concurrency practice changes

---
+++ ConcurrentCollections.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ConcurrentCollections {
+    public static void main(String[] args) {
+
+        // we will get concurrentModificationException when using Arraylist
+        //List<String> list=new ArrayList<>();
+        List<String> list=new CopyOnWriteArrayList<>();
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        list.add("Four");
+        list.add("Five");
+
+        Thread readTread= new Thread(()->{
+            System.out.println("Reading Thread Data:-");
+            for(String str:list){
+                System.out.println("Read: "+str);
+            }
+        });
+
+        Thread writeTread= new Thread(()->{
+            System.out.println("Writing Thread Data:-");
+            list.add("Six");
+        });
+
+        readTread.start();
+        writeTread.start();
+
+    }
+}
